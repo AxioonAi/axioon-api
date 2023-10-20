5be86722702a547401a99e62fd16e4990c1481d1
Gabriel Antonio <mouragabriel205@gmail.com>
2023-10-20 16:04:22 -0400

    remove unnecessary routes in app

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
+import { newsRoutes } from "./http/controller/news/routes";
+import { userRoutes } from "./http/controller/user/routes";
 
 export const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(userRoutes);
+app.register(newsRoutes);
