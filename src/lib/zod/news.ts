523ddca837385b1217e76ad10f537fd97d18dfab
Gabriel Antonio <mouragabriel205@gmail.com>
2023-10-20 16:03:25 -0400

    initial zod config

---
+++ src/lib/zod/news.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const ZodNewsWebhookBodySchema = z.object({
+  Records: z.array(
+    z.object({
+      s3: z.object({
+        object: z.object({
+          key: z.string(),
+        }),
+      }),
+    })
+  ),
+});
