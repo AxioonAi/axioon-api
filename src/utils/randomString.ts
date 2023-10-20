c90f560728c2246c62f4c1d331f0f149ffccd9d1
Gabriel Antonio <mouragabriel205@gmail.com>
2023-10-20 15:59:48 -0400

    file to generate strings to different uses

---
+++ src/utils/randomString.ts
@@ -0,0 +1,13 @@
+export const lettersAndNumbers =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+export const generateRandomString = (length: number) => {
+  let code = "";
+
+  for (let i = 0; i < length; i++) {
+    const random = Math.floor(Math.random() * lettersAndNumbers.length);
+    code += lettersAndNumbers.charAt(random);
+  }
+
+  return code;
+};
