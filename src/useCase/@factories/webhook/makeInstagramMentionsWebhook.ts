0a2f3e37a29392149920189b9d5ea29ba8ec2e73
Gabriel Antonio <mouragabriel205@gmail.com>
2023-11-09 11:05:58 -0400

    fix file name

--- src/useCase/@factories/webhook/makeInstagramMentionsWebhook.ts
+++ src/useCase/@factories/webhook/makeInstagramMentionsWebhook.ts
@@ -1,7 +1,7 @@
 import { AwsNotificationProductionRepository } from "@/repositories/AWS/AwsNotificationProductionRepository";
 import { PrismaInstagramMentionCommentRepository } from "@/repositories/Prisma/PrismaInstagramMentionCommentRepository";
 import { PrismaInstagramMentionRepository } from "@/repositories/Prisma/PrismaInstagramMentionRepository";
-import { InstagramMentionsWebhookUseCase } from "@/useCase/webhook/InstagramMentions";
+import { InstagramMentionsWebhookUseCase } from "@/useCase/webhook/InstagramMentionsWebhook";
 
 export function makeInstagramMentionsWebhook() {
   const awsNotificationRepository = new AwsNotificationProductionRepository();
