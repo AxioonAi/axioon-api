a426a9f3ec871f1335b5faede0e6e8bfa173a740
Gabriel Antonio <mouragabriel205@gmail.com>
2023-11-24 12:47:41 -0400

    advertising controller

---
+++ src/http/controller/politicianProfile/findPoliticianProfileAdvertisingDetails.ts
@@ -0,0 +1,27 @@
+import { ZodIdParamsSchema } from "@/lib/zod/global";
+import { ZodFindPoliticianProfileDetailsQuerySchema } from "@/lib/zod/politicianProfile";
+import { makeFindPoliticianProfileAdvertisingDetails } from "@/useCase/@factories/politicianProfile/makeFindPoliticianProfileAdvertisingDetails";
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export const findPoliticianProfileAdvertisingDetailsController = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const { id } = ZodIdParamsSchema.parse(request.params);
+  const { startDate, endDate } =
+    ZodFindPoliticianProfileDetailsQuerySchema.parse(request.query);
+  try {
+    const findPoliticianProfileAdvertisingDetailsUseCase =
+      makeFindPoliticianProfileAdvertisingDetails();
+
+    const data = await findPoliticianProfileAdvertisingDetailsUseCase.execute({
+      id,
+      startDate,
+      endDate,
+    });
+
+    return reply.status(200).send(data);
+  } catch (error) {
+    throw error;
+  }
+};
