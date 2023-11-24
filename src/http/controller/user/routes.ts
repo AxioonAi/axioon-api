8648339b120d1c82a6e070e016d6b0eb2b69994e
Gabriel Antonio <mouragabriel205@gmail.com>
2023-11-24 12:46:44 -0400

    fix

--- src/http/controller/user/routes.ts
+++ src/http/controller/user/routes.ts
@@ -1,10 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { authenticateUserController } from "./authenticateUser";
 import { registerUserController } from "./registerUser";
-import { websiteUserListController } from "./websiteUserList";
 
 export async function userRoutes(app: FastifyInstance) {
-  app.get("/user/website/:id", websiteUserListController);
   app.post("/login", authenticateUserController);
   app.post("/register", registerUserController);
 }
