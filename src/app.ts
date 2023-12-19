2486005b43c9d799dd6de0eb3a72069c21963325
Gabriel Antonio <mouragabriel205@gmail.com>
2023-12-19 15:42:22 -0400

    add new routes

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,17 @@ import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 import { env } from "./env";
 import { cityRoutes } from "./http/controller/city/routes";
+import { notificationRoutes } from "./http/controller/notification/routes";
 import { politicalGroupRoutes } from "./http/controller/politicalGroup/routes";
-import { politicianProfileRoutes } from "./http/controller/politicianProfile/routes";
+import {
+  politicianProfileMonitoringListRoutes,
+  politicianProfileRoutes,
+} from "./http/controller/politicianProfile/routes";
 import { purchaseRoutes } from "./http/controller/purchase/routes";
 import { scrapeRoutes } from "./http/controller/scrape/routes";
 import { signaturePlanRoutes } from "./http/controller/signaturePlan/routes";
+import { subUserRoutes } from "./http/controller/subUser/routes";
+import { tutorialVideoRoutes } from "./http/controller/tutorialVideo/routes";
 import { userRoutes } from "./http/controller/user/routes";
 import { webhookRoutes } from "./http/controller/webhook/routes";
 import { fastifyErrorHandler } from "./lib/fastify";
@@ -31,9 +37,13 @@ app.register(fastifyJwt, {
 app.register(userRoutes);
 app.register(cityRoutes);
 app.register(scrapeRoutes);
+app.register(subUserRoutes);
 app.register(webhookRoutes);
 app.register(purchaseRoutes);
+app.register(notificationRoutes);
+app.register(tutorialVideoRoutes);
 app.register(signaturePlanRoutes);
 app.register(politicalGroupRoutes);
 app.register(politicianProfileRoutes);
 app.setErrorHandler(fastifyErrorHandler);
+app.register(politicianProfileMonitoringListRoutes);
