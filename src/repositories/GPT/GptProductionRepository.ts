a9de00582823404a926c6e598359440d06ec18f3
Gabriel Antonio <mouragabriel205@gmail.com>
2023-12-20 14:43:50 -0400

    add new gpt functions

--- src/repositories/GPT/GptProductionRepository.ts
+++ src/repositories/GPT/GptProductionRepository.ts
@@ -1,4 +1,7 @@
-import { GptCommentDataInterface } from "@/@types/databaseInterfaces";
+import {
+  GptCommentDataInterface,
+  GptMentionDataInterface,
+} from "@/@types/databaseInterfaces";
 import { SexType } from "@prisma/client";
 import { GptRepository } from "../gptRepository";
 
@@ -9,6 +12,21 @@ export class GptProductionRepository implements GptRepository {
       finalData.push({
         id: item.id,
         authorGender: SexType.FEMALE,
+        sentimentAnalysis: Math.abs(
+          Math.floor(Math.random() * (100 - 1000) + 100)
+        ),
+      });
+    }
+
+    return finalData;
+  }
+
+  async mentionAnalysis(data: GptMentionDataInterface[]) {
+    const finalData = [];
+    for (const item of data) {
+      finalData.push({
+        id: item.id,
+        description: item.description,
         sentimentAnalysis: Math.abs(
           Math.floor(Math.random() * (100 - 1000) + 100)
         ),
--- src/repositories/gptRepository.ts
+++ src/repositories/gptRepository.ts
@@ -1,10 +1,18 @@
 import {
   GptCommentDataInterface,
   GptCommentResponseInterface,
+  GptMentionDataInterface,
+  GptMentionResponseInterface,
+  GptNewsDataInterface,
+  GptNewsResponseInterface,
 } from "@/@types/databaseInterfaces";
 
 export interface GptRepository {
   commentAnalysis(
     data: GptCommentDataInterface[]
   ): Promise<GptCommentResponseInterface[]>;
+  mentionAnalysis(
+    data: GptMentionDataInterface[]
+  ): Promise<GptMentionResponseInterface[]>;
+  newsAnalysis(data: GptNewsDataInterface): Promise<GptNewsResponseInterface>;
 }
