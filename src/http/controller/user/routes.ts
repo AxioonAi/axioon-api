6088b334e7155927c99505e3546a42fb989ddbe9
Gabriel Antonio <mouragabriel205@gmail.com>
2023-12-20 14:40:30 -0400

    update user account route

--- src/http/controller/user/routes.ts
+++ src/http/controller/user/routes.ts
@@ -1,8 +1,15 @@
+import { verifyJwt } from "@/http/middleware/verify-jwt";
 import { FastifyInstance } from "fastify";
 import { authenticateUserController } from "./authenticateUser";
 import { registerUserController } from "./registerUser";
+import { updateUserAccountController } from "./updateUserAccount";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/login", authenticateUserController);
   app.post("/register", registerUserController);
+  app.put(
+    "/user/profile",
+    { onRequest: [verifyJwt] },
+    updateUserAccountController
+  );
 }
---
+++ src/http/controller/user/updateUserAccount.ts
@@ -0,0 +1,21 @@
+import { ZodUpdateUserAccountBodySchema } from "@/lib/zod/user";
+import { makeUpdateUserAccount } from "@/useCase/@factories/user/makeUpdateUserAccount";
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export const updateUserAccountController = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const data = ZodUpdateUserAccountBodySchema.parse(request.body);
+
+  try {
+    const updateUserAccountUseCase = makeUpdateUserAccount();
+
+    await updateUserAccountUseCase.execute({
+      data,
+      id: request.user.sub,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
