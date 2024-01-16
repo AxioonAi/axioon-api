dec6767b6b70f4d7efd827494fcd31d09f7f5882
GabrielFernandes123 <mouragabriel205@gmail.com>
2024-01-16 13:00:30 -0400

    add missing repository

--- src/useCase/@factories/politicianProfile/makeCityStatistics.ts
+++ src/useCase/@factories/politicianProfile/makeCityStatistics.ts
@@ -1,9 +1,12 @@
 import { PrismaPoliticianProfileRepository } from "@/repositories/Prisma/PrismaPoliticianProfileRepository";
+import { PrismaUserPlanRepository } from "@/repositories/Prisma/PrismaUserPlanRepository";
 import { FindPoliticianProfileCityDetailsUseCase } from "@/useCase/politicianProfile/FindPoliticianProfileCityDetails";
 
 export function makeFindPoliticianProfileCityDetails() {
 	const politicianProfileRepository = new PrismaPoliticianProfileRepository();
+	const userPlanRepository = new PrismaUserPlanRepository();
 	return new FindPoliticianProfileCityDetailsUseCase(
 		politicianProfileRepository,
+		userPlanRepository,
 	);
 }
