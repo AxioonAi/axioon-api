25180d5ba16a8e5a2916810bf5d135244865389a
GabrielFernandes123 <85517869+GabrielFernandes123@users.noreply.github.com>
2024-03-01 13:52:11 -0400

    fix tiktok profile list

--- src/useCase/politicianProfile/FindPoliticianProfileTiktokDetails.ts
+++ src/useCase/politicianProfile/FindPoliticianProfileTiktokDetails.ts
@@ -6,99 +6,101 @@ import { tiktokDataFormatter } from "@/utils/dataFormatter/tiktok";
 import moment from "moment";
 
 interface FindPoliticianProfileTiktokDetailsUseCaseRequest {
-	id: string;
-	period: number;
+  id: string;
+  period: number;
 }
 
 interface FindPoliticianProfileTiktokDetailsUseCaseResponse {
-	data: {
-		keyIndicators: {
-			name: string;
-			current: number;
-			previous: number;
-		}[];
-		commentsStatistics: {
-			commentTime: {
-				name: string;
-				value: number;
-			}[];
-			sentimentStatistics: {
-				totalSentiment: number;
-				countSentiment0To350: number;
-				countSentiment351To650: number;
-				countSentiment651To1000: number;
-				sentimentAverage: number;
-			};
-		};
-		posts: TiktokDataFormatterFinalDataInterface[];
-	};
+  data: {
+    keyIndicators: {
+      name: string;
+      current: number;
+      previous: number;
+    }[];
+    commentsStatistics: {
+      commentTime: {
+        name: string;
+        value: number;
+      }[];
+      sentimentStatistics: {
+        totalSentiment: number;
+        countSentiment0To350: number;
+        countSentiment351To650: number;
+        countSentiment651To1000: number;
+        sentimentAverage: number;
+      };
+    };
+    posts: TiktokDataFormatterFinalDataInterface[];
+  };
 }
 
 export class FindPoliticianProfileTiktokDetailsUseCase {
-	constructor(
-		private politicianProfileRepository: PoliticianProfileRepository,
-	) {}
+  constructor(
+    private politicianProfileRepository: PoliticianProfileRepository
+  ) {}
 
-	async execute({
-		id,
-		period,
-	}: FindPoliticianProfileTiktokDetailsUseCaseRequest): Promise<FindPoliticianProfileTiktokDetailsUseCaseResponse> {
-		const [current, previous] = await Promise.all([
-			this.politicianProfileRepository.findTiktokStatistics({
-				id,
-				gte: moment().subtract(period, "day").toDate(),
-				lte: moment().toDate(),
-			}),
-			this.politicianProfileRepository.findTiktokStatistics({
-				id,
-				gte: moment()
-					.subtract(period * 2, "day")
-					.toDate(),
-				lte: moment()
-					.subtract(period - 1, "day")
-					.toDate(),
-			}),
-		]);
+  async execute({
+    id,
+    period,
+  }: FindPoliticianProfileTiktokDetailsUseCaseRequest): Promise<FindPoliticianProfileTiktokDetailsUseCaseResponse> {
+    const [current, previous] = await Promise.all([
+      this.politicianProfileRepository.findTiktokStatistics({
+        id,
+        gte: moment().subtract(period, "day").toDate(),
+        lte: moment().toDate(),
+      }),
+      this.politicianProfileRepository.findTiktokStatistics({
+        id,
+        gte: moment()
+          .subtract(period * 2, "day")
+          .toDate(),
+        lte: moment()
+          .subtract(period - 1, "day")
+          .toDate(),
+      }),
+    ]);
 
-		if (!current || !previous) throw new ProfileNotFoundError();
+    console.log(current);
 
-		if (current.tiktokData.length === 0 && previous.tiktokData.length === 0) {
-			throw new DataNotFoundError();
-		}
-		if (current.tiktokVideoData.length === 0) {
-			throw new DataNotFoundError();
-		}
+    if (!current || !previous) throw new ProfileNotFoundError();
 
-		const formatCurrent = tiktokDataFormatter(current, previous.tiktokData[0]);
-		const formatPrevious = tiktokDataFormatter(previous, current.tiktokData[0]);
+    if (current.tiktokData.length === 0 && previous.tiktokData.length === 0) {
+      throw new DataNotFoundError();
+    }
+    if (current.tiktokVideoData.length === 0) {
+      throw new DataNotFoundError();
+    }
 
-		const finalStatistics = {
-			keyIndicators: [
-				{
-					name: "Curtidas",
-					current: formatCurrent.videoEngagementData.like,
-					previous: formatPrevious.videoEngagementData.like,
-				},
-				{
-					name: "Comentários",
-					current: formatCurrent.videoEngagementData.comments,
-					previous: formatPrevious.videoEngagementData.comments,
-				},
-				{
-					name: "Visualizações",
-					current: formatCurrent.videoEngagementData.views,
-					previous: formatPrevious.videoEngagementData.views,
-				},
-				{
-					name: "Sentimento",
-					current: formatCurrent.videoEngagementData.sentiment,
-					previous: formatPrevious.videoEngagementData.sentiment,
-				},
-			],
-			commentsStatistics: formatCurrent.commentsStatistics,
-			posts: formatPrevious.videos,
-		};
+    const formatCurrent = tiktokDataFormatter(current, previous.tiktokData[0]);
+    const formatPrevious = tiktokDataFormatter(previous, current.tiktokData[0]);
 
-		return { data: finalStatistics };
-	}
+    const finalStatistics = {
+      keyIndicators: [
+        {
+          name: "Curtidas",
+          current: formatCurrent.videoEngagementData.like,
+          previous: formatPrevious.videoEngagementData.like,
+        },
+        {
+          name: "Comentários",
+          current: formatCurrent.videoEngagementData.comments,
+          previous: formatPrevious.videoEngagementData.comments,
+        },
+        {
+          name: "Visualizações",
+          current: formatCurrent.videoEngagementData.views,
+          previous: formatPrevious.videoEngagementData.views,
+        },
+        {
+          name: "Sentimento",
+          current: formatCurrent.videoEngagementData.sentiment,
+          previous: formatPrevious.videoEngagementData.sentiment,
+        },
+      ],
+      commentsStatistics: formatCurrent.commentsStatistics,
+      posts: formatCurrent.videos,
+    };
+
+    return { data: finalStatistics };
+  }
 }
