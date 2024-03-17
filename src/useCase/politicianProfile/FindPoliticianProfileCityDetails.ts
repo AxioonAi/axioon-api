3a67dcd6eebcaa2c89aa39bd80b14ef362bf9d05
GabrielFernandes123 <85517869+GabrielFernandes123@users.noreply.github.com>
2024-03-17 14:06:58 -0400

    fix city data

--- src/useCase/politicianProfile/FindPoliticianProfileCityDetails.ts
+++ src/useCase/politicianProfile/FindPoliticianProfileCityDetails.ts
@@ -1,75 +1,74 @@
-import { UnauthorizedError } from "@/helper/errors/UnauthorizedError";
 import { UserNotFoundError } from "@/helper/errors/UserNotFoundError";
 import { PoliticianProfileRepository } from "@/repositories/PoliticianProfileRepository";
 import { UserPlanRepository } from "@/repositories/UserPlanRepository";
 import { CityDataFormatter } from "@/utils/dataFormatter/city";
 
 interface FindPoliticianProfileCityDetailsUseCaseRequest {
-	id: string;
-	userId: string;
+  id: string;
+  userId: string;
 }
 
 interface FindPoliticianProfileCityDetailsUseCaseResponse {
-	city: {
-		id: string;
-		name: string;
-		state: string;
-		electorate: {
-			total: number;
-			gender: {
-				name: string;
-				value: number;
-			}[];
-			with_disability: number;
-			with_biometry: number;
-			schoolLevel: {
-				name: string;
-				value: number;
-			}[];
-			ageRange: {
-				name: string;
-				value: number;
-			}[];
-		};
-		population: {
-			total: number;
-			male: number;
-			female: number;
-			ageRange: {
-				name: string;
-				total: number;
-				Homens: number;
-				Mulheres: number;
-			}[];
-		};
-		politician: number;
-	};
+  city: {
+    id: string;
+    name: string;
+    state: string;
+    electorate: {
+      total: number;
+      gender: {
+        name: string;
+        value: number;
+      }[];
+      with_disability: number;
+      with_biometry: number;
+      schoolLevel: {
+        name: string;
+        value: number;
+      }[];
+      ageRange: {
+        name: string;
+        value: number;
+      }[];
+    };
+    population: {
+      total: number;
+      male: number;
+      female: number;
+      ageRange: {
+        name: string;
+        total: number;
+        Homens: number;
+        Mulheres: number;
+      }[];
+    };
+    politician: number;
+  };
 }
 
 export class FindPoliticianProfileCityDetailsUseCase {
-	constructor(
-		private politicianProfileRepository: PoliticianProfileRepository,
-		private userPlanRepository: UserPlanRepository,
-	) {}
+  constructor(
+    private politicianProfileRepository: PoliticianProfileRepository,
+    private userPlanRepository: UserPlanRepository
+  ) {}
 
-	async execute({
-		userId,
-		id,
-	}: FindPoliticianProfileCityDetailsUseCaseRequest): Promise<FindPoliticianProfileCityDetailsUseCaseResponse> {
-		const userPlan = await this.userPlanRepository.findActivePlan(userId);
+  async execute({
+    userId,
+    id,
+  }: FindPoliticianProfileCityDetailsUseCaseRequest): Promise<FindPoliticianProfileCityDetailsUseCaseResponse> {
+    // const userPlan = await this.userPlanRepository.findActivePlan(userId);
 
-		if (!userPlan || !userPlan.plan.facebook_ads_monitoring) {
-			throw new UnauthorizedError();
-		}
+    // if (!userPlan || !userPlan.plan.facebook_ads_monitoring) {
+    // 	throw new UnauthorizedError();
+    // }
 
-		const user = await this.politicianProfileRepository.findUserCity(id);
+    const user = await this.politicianProfileRepository.findUserCity(id);
 
-		if (!user) {
-			throw new UserNotFoundError();
-		}
+    if (!user) {
+      throw new UserNotFoundError();
+    }
 
-		const formattedData = CityDataFormatter(user);
+    const formattedData = CityDataFormatter(user);
 
-		return { city: formattedData };
-	}
+    return { city: formattedData };
+  }
 }
