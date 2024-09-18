906a64f1bf51d9a756d85f2eb51e311d06aeddc9
GabrielFernandes123 <85517869+GabrielFernandes123@users.noreply.github.com>
2024-09-18 13:07:09 -0400

    add route do edit profile

--- src/http/controller/politicianProfile/routes.ts
+++ src/http/controller/politicianProfile/routes.ts
@@ -4,48 +4,27 @@ import { createPoliticianProfileController } from "./createPoliticianProfile";
 import { createPoliticianProfileMonitoringController } from "./createPoliticianProfileMonitoring";
 import { findPoliticianProfileAdvertisingDetailsController } from "./findPoliticianProfileAdvertisingDetails";
 import { findPoliticianProfileCityDetailsController } from "./findPoliticianProfileCityDetails";
-// import { findPoliticianProfileFacebookDetailsController } from "./findPoliticianProfileFacebookDetails";
-// import { findPoliticianProfileInstagramDetailsController } from "./findPoliticianProfileInstagramDetails";
 import { findPoliticianProfileLegalDetailsController } from "./findPoliticianProfileLegalDetails";
 import { findPoliticianProfileMentionDetailsController } from "./findPoliticianProfileMentionDetails";
 import { findPoliticianProfileMonitoringListController } from "./findPoliticianProfileMonitoringList";
-// import { findPoliticianProfileSocialMediaHomeDataController } from "./findPoliticianProfileSocialMediaHomeData";
-// import { findPoliticianProfileTiktokDetailsController } from "./findPoliticianProfileTiktokDetails";
-// import { findPoliticianProfileYoutubeDetailsController } from "./findPoliticianProfileYoutubeDetails";
 import { politicianProfileExistsController } from "./politicianProfileExists";
 import { fetchAllProfilesEngagementController } from "./fetchAllProfilesEngagement";
 import { findPoliticianProfileSocialMediaDataController } from "./findPoliticianProfileSocialMediaData";
 import { removeProfileMonitoringController } from "./removeProfileMonitoring";
+import { editPoliticianProfileController } from "./editPoliticianProfile";
 
 export async function politicianProfileRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJwt);
-  // app.addHook("onRequest", verifyAccessMiddleware);
 
   app.post("/profile/exists", politicianProfileExistsController);
   app.post("/profile", createPoliticianProfileController);
-  // app.get(
-  //   "/profile/youtube/:id",
-  //   findPoliticianProfileYoutubeDetailsController
-  // );
-  // app.get(
-  //   "/profile/social/home/:id",
-  //   findPoliticianProfileSocialMediaHomeDataController
-  // );
-  // app.get("/profile/tiktok/:id", findPoliticianProfileTiktokDetailsController);
-
-  // app.get(
-  //   "/profile/facebook/:id",
-  //   findPoliticianProfileFacebookDetailsController
-  // );
+  app.put("/profile", editPoliticianProfileController);
 
   app.get(
     "/profile/advertising/:id",
     findPoliticianProfileAdvertisingDetailsController
   );
-  // app.get(
-  //   "/profile/instagram/:id",
-  //   findPoliticianProfileInstagramDetailsController
-  // );
+
   app.get(
     "/profile/mentions/:id",
     findPoliticianProfileMentionDetailsController
